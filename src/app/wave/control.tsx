eb0c89d48a2d29967187b88dc56bf753c2442d45
jackcipher <jackcipher.vip@gmail.com>
2023-12-29 00:38:04 +0800

    feature: add player controls

--- src/app/wave/control.tsx
+++ src/app/wave/control.tsx
@@ -1,13 +1,23 @@
-import { PauseCircleOutlined, PlayCircleFilled } from "@ant-design/icons";
-
+import {
+  EnvironmentFilled,
+  FastBackwardFilled,
+  FastForwardFilled,
+} from "@ant-design/icons";
 import { Flex, Slider } from "antd";
 import { useState } from "react";
 import styles from "./control.module.scss";
-import ABPlayIcon from "./svg/ab.svg";
+import BackwardIcon from "./svg/backward.svg";
+import ForwardIcon from "./svg/forward.svg";
+import PauseIcon from "./svg/pause.svg";
+import PlayIcon from "./svg/play.svg";
 
 interface ControlProp {
-  onZoomChange(v: number): void;
+  onZoomChange?(v: number): void;
   onPlayPauseClick(): void;
+  onForwardBackwardClick(offset: number): void;
+  onLoopSetClick(): void;
+  onNextLoopClick(): void;
+  onPrevLoopClick(): void;
 }
 
 const primaryBtnClass = styles.btn + " " + styles.primary;
@@ -21,34 +31,51 @@ const Control = (prop: ControlProp) => {
   return (
     <>
       <Flex className={styles.wrapper} vertical align="center">
-        <Flex gap="small">
-          {/* <PlayCircleFilled className={styles.btn} /> */}
-          {!isPlaying && (
-            <PlayCircleFilled
-              className={primaryBtnClass}
-              onClick={onPlayPauseClick}
-            />
-          )}
-          {isPlaying && (
-            <PauseCircleOutlined
-              className={primaryBtnClass}
-              onClick={onPlayPauseClick}
-            />
-          )}
-
-          <ABPlayIcon />
-        </Flex>
         <Flex>
-          <Slider
-            style={{ width: 120 }}
-            min={10}
-            max={200}
-            onChange={(v) => {
-              prop.onZoomChange(v);
-            }}
-            defaultValue={50}
-          />
+          <div className={styles.btn}>
+            <FastBackwardFilled />
+          </div>
+          <div className={styles.btn}>
+            <EnvironmentFilled />
+          </div>
+          <div className={styles.btn}>
+            <FastForwardFilled />
+          </div>
         </Flex>
+        <Flex align="center">
+          <div
+            className={primaryBtnClass}
+            onClick={() => {
+              prop.onForwardBackwardClick(-3);
+            }}
+          >
+            <BackwardIcon />
+          </div>
+          <div className={primaryBtnClass} onClick={onPlayPauseClick}>
+            {!isPlaying ? <PlayIcon /> : <PauseIcon />}
+          </div>
+          <div
+            className={primaryBtnClass}
+            onClick={() => {
+              prop.onForwardBackwardClick(5);
+            }}
+          >
+            <ForwardIcon />
+          </div>
+        </Flex>
+        {prop.onZoomChange && (
+          <Flex>
+            <Slider
+              style={{ width: 120 }}
+              min={10}
+              max={200}
+              onChange={(v) => {
+                prop.onZoomChange!(v);
+              }}
+              defaultValue={50}
+            />
+          </Flex>
+        )}
       </Flex>
     </>
   );
