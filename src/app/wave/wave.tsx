79b48e65e034be3ce4c6790d3b5c82a3154eb38a
xuwei <xuwei@baijiayun.com>
2023-12-29 01:06:44 +0800

    fix: merge conflict

--- src/app/wave/wave.tsx
+++ src/app/wave/wave.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import WaveSurferPlayer from "@/components/waveform/waveform";
-import { Button, Flex, Progress, Slider, Spin } from "antd";
+import { Button, Spin } from "antd";
 import { useEffect, useRef, useState } from "react";
 import WaveSurfer from "wavesurfer.js";
+import RegionsPlugin from "wavesurfer.js/dist/plugins/regions.js";
 import TimelinePlugin from "wavesurfer.js/dist/plugins/timeline.js";
 import Control from "./control";
 
@@ -18,93 +19,85 @@ const Home = () => {
     },
   });
 
-  const [loadPercent, setLoadPercent] = useState(0);
-  const [playBtnText, setPlayBtnText] = useState("Play");
   const [loaded, setLoaded] = useState(false);
   const [ws, setWavesurfer] = useState<WaveSurfer>();
+  // const [wsRegion, setWsRegion] = useState<RegionsPlugin>(null);
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {}, [containerRef]);
 
   return (
     <>
-      <Control
-        onPlayPauseClick={() => {
-          ws!.playPause();
-        }}
-        onZoomChange={(v) => {
-          ws?.zoom(v);
-        }}
-      ></Control>
-
-      <Spin spinning={!loaded}>
-        <Flex vertical>
-          <Flex>
-            <Progress percent={loadPercent} />
-          </Flex>
-          <Flex align="center" gap="middle">
-            Zoom:
-            <Slider
-              style={{ width: 200 }}
-              min={10}
-              max={200}
-              onChange={(v) => {
-                ws?.zoom(v);
-              }}
-              defaultValue={50}
-            />
-          </Flex>
-
-          <Flex align="center" gap="middle">
-            Play Control:
-            <Button
-              onClick={() => {
-                ws!.setTime(ws!.getCurrentTime() - 3);
-              }}
-            >
-              Backward 3s
-            </Button>
-            <Button
-              onClick={() => {
-                ws!.setTime(ws!.getCurrentTime() + 5);
-              }}
-            >
-              Forward 5s
-            </Button>
-            <Button
-              onClick={() => {
-                ws!.playPause();
-                if (ws!.isPlaying()) {
-                  setPlayBtnText("Pause");
-                } else {
-                  setPlayBtnText("Play");
-                }
-              }}
-            >
-              {playBtnText}
-            </Button>
-          </Flex>
-        </Flex>
-      </Spin>
-
       <Spin spinning={!loaded}>
         <WaveSurferPlayer
-          setWavesurfer={(v) => {
-            setWavesurfer(v);
+          setWavesurfer={(wavesurfer) => {
+            setWavesurfer(wavesurfer);
+            // setWsRegion(wavesurfer.registerPlugin(RegionsPlugin.create()));
             setLoaded(true);
           }}
           url="/demo.mp3"
           height={100}
           waveColor="#C4C4C4"
           progressColor="#FE6E00"
-          autoScroll={true}
           autoCenter={true}
-          hideScrollbar={true}
+          autoScroll={true}
           dragToSeek={true}
+          hideScrollbar={true}
           minPxPerSec={50}
-          plugins={[bottomTimline]}
+          plugins={[bottomTimline, RegionsPlugin.create()]}
         />
       </Spin>
+
+      <Spin spinning={!loaded}>
+        <Control
+          onPlayPauseClick={() => {
+            ws!.playPause();
+          }}
+          onForwardBackwardClick={(offset) => {
+            ws!.setTime(ws!.getCurrentTime() + offset);
+          }}
+          onLoopSetClick={() => {}}
+          onNextLoopClick={() => {}}
+          onPrevLoopClick={() => {}}
+        ></Control>
+      </Spin>
+      <Button
+        onClick={() => {
+          ws!.getActivePlugins().forEach((v) => {
+            if ("regions" in v) {
+              //@ts-ignore
+              const wsRegion: RegionsPlugin = v;
+              wsRegion.addRegion({ start: 5 });
+              wsRegion.addRegion({ start: 15 });
+              wsRegion.addRegion({ start: 10 });
+              wsRegion.addRegion({ start: 30 });
+              wsRegion.addRegion({ start: 20.123432131231231 });
+              wsRegion.addRegion({ start: 2 });
+            }
+          });
+        }}
+      >
+        add region
+      </Button>
+      <Button
+        onClick={() => {
+          ws!.getActivePlugins().forEach((v) => {
+            if ("regions" in v) {
+              //@ts-ignore
+              const wsRegion: RegionsPlugin = v;
+              const regions = wsRegion
+                .getRegions()
+                .sort((a, b) => a.end - b.end);
+              console.log(regions);
+              regions.forEach((x) => {
+                console.log(x.end);
+              });
+            }
+          });
+        }}
+      >
+        show regions
+      </Button>
     </>
   );
 };
